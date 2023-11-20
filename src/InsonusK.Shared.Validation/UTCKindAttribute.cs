3cb93f72c10fe8e0f0eac304fc583980e90d9121
InsonusK <insonus.k@gmail.com>
2023-11-20 03:28:38 +0000

    init

---
+++ src/InsonusK.Shared.Validation/UTCKindAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InsonusK.Shared.Validation;
+
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter, AllowMultiple = false)]
+public class UTCKindAttribute : ValidationAttribute
+{
+  public override bool RequiresValidationContext => false;
+  protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+  {
+    var dt = value is DateTime dateTimeValue ? dateTimeValue : throw new ArgumentException("Value type must be DateTime");
+    if (IsUTC(dt))
+      return ValidationResult.Success;
+
+    return new ValidationResult($"DateTime must be in UTC kind", new string[] { validationContext?.MemberName! });
+  }
+
+  public static bool IsUTC(DateTime dt)
+  {
+    return dt.Kind == DateTimeKind.Utc;
+  }
+
+  public static bool IsNotUTC(DateTime dt)
+  {
+    return !IsUTC(dt);
+  }
+}
---
+++ test/InsonusK.Shared.Validation.Test/UTCKindAttribute.Test.cs
@@ -0,0 +1,69 @@
+//using InsonusK.Xunit.ExpectationsTest;
+using System.ComponentModel.DataAnnotations;
+using InsonusK.Xunit.ExpectationsTest;
+using Microsoft.Extensions.Logging;
+using Xunit.Abstractions;
+
+namespace InsonusK.Shared.Validation.Test;
+
+public class UTCKindAttributeTest : ExpectationsTestBase
+{
+    public UTCKindAttributeTest(ITestOutputHelper output, LogLevel logLevel = LogLevel.Debug) : base(output, logLevel)
+    {
+    }
+
+    [Fact]
+    public void WHEN_dt_is_UTC_THEN_Ok()
+    {
+        #region Array
+        Logger.LogDebug("Test ARRAY");
+
+        var using_dt = DateTime.UtcNow;
+
+        var usingAtr = new UTCKindAttribute();
+        #endregion
+
+
+        #region Act
+        Logger.LogDebug("Test ACT");
+
+        var asserted_result = usingAtr.IsValid(using_dt);
+
+        #endregion
+
+
+        #region Assert
+        Logger.LogDebug("Test ASSERT");
+
+        Expect("Expect is valid", () => Assert.True(asserted_result));
+
+        #endregion
+    }
+
+    [Fact]
+    public void WHEN_dt_is_not_UTC_THEN_Ok()
+    {
+        #region Array
+        Logger.LogDebug("Test ARRAY");
+
+        var using_dt = DateTime.Now;
+        var usingAtr = new UTCKindAttribute();
+        #endregion
+
+
+        #region Act
+        Logger.LogDebug("Test ACT");
+
+        var asserted_result = usingAtr.IsValid(using_dt);
+
+        #endregion
+
+
+        #region Assert
+        Logger.LogDebug("Test ASSERT");
+
+        Expect("Expect is not valid", () => Assert.False(asserted_result));
+
+        #endregion
+    }
+}
